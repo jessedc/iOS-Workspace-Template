1feed0dfc68ea16af73802436eefcffe44e05814
Jesse Collis <jesse@jcmultimedia.com.au>
2013-05-30 17:30:59 +1000

    Setup a test loader target for static lib unit tests with Kiwi

---
+++ common/StaticLib/resources/StaticLibTestLoaderMain.m
@@ -0,0 +1,11 @@
+
+#import <UIKit/UIKit.h>
+
+#import "StaticLibTestLoaderAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
